41e4645572a07c6da0b29c72e6f705a2d30212ea
Codist <countstarlight@gmail.com>
2021-08-25 17:17:25 +0800

    Upgrade deepin base package to 3.2.1.154deepin13 and use aur package 'deepin-wine6-stable' instead of wine.
    Signed-off-by: Codist <countstarlight@gmail.com>

--- PKGBUILD
+++ PKGBUILD
@@ -3,14 +3,14 @@
 pkgname=deepin-wine-wechat
 pkgver=3.3.5.42
 wechat_installer=WeChatSetup
-deepinwechatver=3.2.1.154deepin8
+deepinwechatver=3.2.1.154deepin13
 debpkgname="com.qq.weixin.deepin"
-pkgrel=1
+pkgrel=2
 pkgdesc="Tencent WeChat on Deepin Wine(${debpkgname}) For Archlinux"
 arch=("x86_64")
 url="https://weixin.qq.com/"
 license=('custom')
-depends=('p7zip' 'wine' 'wine-mono' 'wine-gecko' 'xorg-xwininfo' 'wqy-microhei' 'lib32-alsa-lib' 'lib32-alsa-plugins' 'lib32-libpulse' 'lib32-openal' 'lib32-mpg123' 'lib32-libldap')
+depends=('p7zip' 'deepin-wine6-stable' 'deepin-wine-helper' 'xorg-xwininfo' 'wqy-microhei' 'lib32-alsa-lib' 'lib32-alsa-plugins' 'lib32-libpulse' 'lib32-openal' 'lib32-mpg123' 'lib32-libldap')
 conflicts=('deepin-wechat')
 install="deepin-wine-wechat.install"
 _mirror="https://com-store-packages.uniontech.com"
@@ -18,9 +18,9 @@ source=("$_mirror/appstore/pool/appstore/c/${debpkgname}/${debpkgname}_${deepinw
   "${wechat_installer}-${pkgver}.exe::https://dldir1.qq.com/weixin/Windows/${wechat_installer}.exe"
   "run.sh"
   "reg.patch")
-md5sums=('3246f43cdfd2a4de9597d7018cb21753'
+md5sums=('1cc7cfc9f77f60147f476121bd9e7542'
          '4204a40f3b9f4123c5347365a453473e'
-         'fe6d9165926762788de03e9c67038c0a'
+         '27b3ff80e6d8cc058bbd0aa1be5348c6'
          'f3257f8fc9e73ea88b3a46372634f82f')
 
 build() {
@@ -52,9 +52,10 @@ package() {
   cp -r "${srcdir}/dpkgdir/opt/apps/${debpkgname}/entries/icons/" "${pkgdir}/usr/share/"
   msg "Copying deepin files ..."
   install -d "${pkgdir}/opt/apps/${debpkgname}/files"
+  cp -r "${srcdir}/dpkgdir/opt/apps/${debpkgname}/files/dlls" "${pkgdir}/opt/apps/${debpkgname}/files/"
   install -m644 "${srcdir}/files.7z" "${pkgdir}/opt/apps/${debpkgname}/files/"
-  cp ${srcdir}/dpkgdir/opt/apps/${debpkgname}/files/helper_archive* "${pkgdir}/opt/apps/${debpkgname}/files/"
-  install -m755 "${srcdir}/dpkgdir/opt/apps/${debpkgname}/files/gtkGetFileNameDlg" "${pkgdir}/opt/apps/${debpkgname}/files/"
+  # cp ${srcdir}/dpkgdir/opt/apps/${debpkgname}/files/helper_archive* "${pkgdir}/opt/apps/${debpkgname}/files/"
+  # install -m755 "${srcdir}/dpkgdir/opt/apps/${debpkgname}/files/gtkGetFileNameDlg" "${pkgdir}/opt/apps/${debpkgname}/files/"
   md5sum "${srcdir}/files.7z" | awk '{ print $1 }' > "${pkgdir}/opt/apps/${debpkgname}/files/files.md5sum"
   install -m755 "${srcdir}/run.sh" "${pkgdir}/opt/apps/${debpkgname}/files/"
 }
--- run.sh
+++ run.sh
@@ -9,108 +9,52 @@
 
 version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 
-extract_archive()
-{
-    archive=$1
-    version_file=$2
-    dest_dir=$3
-    if [ -f "$archive" ] && [ -n "$dest_dir" ] && [ "$dest_dir" != "." ];then
-        archive_version=`cat $version_file`
-        if [ -d "$dest_dir" ];then
-            if [ -f "$dest_dir/VERSION" ];then
-                dest_version=`cat $dest_dir/VERSION`
-                if version_gt "$archive_version" "$dest_version" || [ -z "$dest_version" ];then
-                    7z x "$archive" -o/"$dest_dir" -aoa
-                    echo "$archive_version" > "$dest_dir/VERSION"
-                fi
-            fi
-        else
-            mkdir -p $dest_dir
-            7z x "$archive" -o/"$dest_dir" -aoa
-            echo "$archive_version" > "$dest_dir/VERSION"
-        fi
-    fi
-}
-
 BOTTLENAME="Deepin-WeChat"
-APPVER="3.2.1.154deepin8"
+APPVER="3.2.1.154deepin13"
 WINEPREFIX="$HOME/.deepinwine/$BOTTLENAME"
 WECHAT_VER="3.3.5.42"
 EXEC_PATH="c:/Program Files/Tencent/WeChat/WeChat.exe"
-START_SHELL_PATH="$HOME/.deepinwine/deepin-wine-helper/run_v3.sh"
+START_SHELL_PATH="/opt/deepinwine/tools/run_v4.sh"
 WECHAT_INSTALLER="WeChatSetup"
 WECHAT_INSTALLER_PATH="c:/Program Files/Tencent/$WECHAT_INSTALLER-$WECHAT_VER.exe"
 export MIME_TYPE=""
 export DEB_PACKAGE_NAME="com.qq.weixin.deepin"
-#export APPRUN_CMD="wine"
-export PATCH_LOADER_ENV=""
-export FILEDLG_PLUGIN="/opt/apps/$DEB_PACKAGE_NAME/files/gtkGetFileNameDlg"
+export APPRUN_CMD="deepin-wine6-stable"
+DISABLE_ATTACH_FILE_DIALOG=""
 
 export SPECIFY_SHELL_DIR=`dirname $START_SHELL_PATH`
 
 ARCHIVE_FILE_DIR="/opt/apps/$DEB_PACKAGE_NAME/files"
 
-msg()
-{
-	ECHO_LEVEL=("\033[1;32m==> " "\033[1;31m==> ERROR: ")
-	echo -e "${ECHO_LEVEL[$1]}\033[1;37m$2\033[0m"
-}
+export WINEDLLPATH=/opt/$APPRUN_CMD/lib:/opt/$APPRUN_CMD/lib64
 
-SwitchToDeepinWine()
-{
-    PACKAGE_MANAGER="yay"
-    DEEPIN_WINE_DEPENDS="deepin-wine5"
-    if ! [ -x "$(command -v yay)" ]; then
-        if ! [ -x "$(command -v yaourt)" ]; then
-            msg 1 "Need to install 'yay' or 'yaourt' first." >&2
-            exit 1
-        else
-            PACKAGE_MANAGER="yaourt"
-        fi
-    fi
-    for p in ${DEEPIN_WINE_DEPENDS}; do
-        if pacman -Qs $p > /dev/null ; then
-            msg 0 "$p is installed, skip ..."
-        else
-            msg 0 "Installing dependency: $p ..."
-            $PACKAGE_MANAGER -S $p
-        fi
-    done
-    msg 0 "Redeploying app ..."
-    extract_archive "$ARCHIVE_FILE_DIR/helper_archive.7z" "$ARCHIVE_FILE_DIR/helper_archive.md5sum" "$SPECIFY_SHELL_DIR"
-    $START_SHELL_PATH $BOTTLENAME $APPVER "$EXEC_PATH" -r
-    echo "5" > $WINEPREFIX/deepin
-    rm -f $WINEPREFIX/reinstalled
-    msg 0 "Done."
-    exit 0
-}
+export WINEPREDLL="$ARCHIVE_FILE_DIR/dlls"
 
 OpenWinecfg()
 {
-    if [ -f "$WINEPREFIX/deepin" ]; then
-        env WINEPREFIX=$WINEPREFIX deepin-wine5 winecfg
-    else
-        env WINEPREFIX=$WINEPREFIX winecfg
-    fi
+    env WINEPREFIX=$WINEPREFIX $APPRUN_CMD winecfg
 }
 
 Run()
 {
-    extract_archive "$ARCHIVE_FILE_DIR/helper_archive.7z" "$ARCHIVE_FILE_DIR/helper_archive.md5sum" "$SPECIFY_SHELL_DIR"
-
-    if [ -n "$PATCH_LOADER_ENV" ] && [ -n "$EXEC_PATH" ];then
-        export $PATCH_LOADER_ENV
+    if [ -z "$DISABLE_ATTACH_FILE_DIALOG" ];then
+        export ATTACH_FILE_DIALOG=1
     fi
 
     if [ -n "$EXEC_PATH" ];then
         if [ ! -f "$WINEPREFIX/reinstalled" ];then
+            # run installer
             touch $WINEPREFIX/reinstalled
             env WINEDLLOVERRIDES="winemenubuilder.exe=d" $START_SHELL_PATH $BOTTLENAME $APPVER "$WECHAT_INSTALLER_PATH" "$@"
             if [ $APPRUN_CMD = "deepin-wine5" ]; then
                 echo "5" > $WINEPREFIX/deepin
             fi
         else
-            $START_SHELL_PATH $BOTTLENAME $APPVER "$EXEC_PATH" "$@"
+            if [ -z "${EXEC_PATH##*.lnk*}" ];then
+                $START_SHELL_PATH $BOTTLENAME $APPVER "C:/windows/command/start.exe" "/Unix" "$EXEC_PATH" "$@"
+            else
+                $START_SHELL_PATH $BOTTLENAME $APPVER "$EXEC_PATH" "$@"
+            fi
         fi
     else
         $START_SHELL_PATH $BOTTLENAME $APPVER "uninstaller.exe" "$@"
@@ -121,20 +65,8 @@ HelpApp()
 {
 	echo " Extra Commands:"
 	echo " winecfg        Open winecfg"
-	echo " -d/--deepin    Switch to 'deepin-wine'"
 	echo " -h/--help      Show program help info"
 }
-
-if [ -f "$WINEPREFIX/deepin" ]; then
-    if [ "$(cat $WINEPREFIX/deepin)" = "5" ]; then
-        export APPRUN_CMD="deepin-wine5"
-    else
-        rm $WINEPREFIX/deepin
-        export APPRUN_CMD="wine"
-    fi
-else
-    export APPRUN_CMD="wine"
-fi
 
 if [ -z $1 ]; then
 	Run "$@"
@@ -143,9 +75,6 @@ fi
 case $1 in
 	"winecfg")
 		OpenWinecfg
-		;;
-	"-d" | "--deepin")
-		SwitchToDeepinWine
 		;;
 	"-h" | "--help")
 		HelpApp

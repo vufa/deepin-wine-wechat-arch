e64fadb7c712e047e0bdc19317e0d208d4035b9c
Vufa <countstarlight@gmail.com>
2022-06-02 12:54:54 +0800

    Upgrade WeChat to 3.7.0.29
    Signed-off-by: Vufa <countstarlight@gmail.com>

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ BOTTLENAME="Deepin-WeChat"
 APPVER="3.4.0.38deepin6"
 WINEPREFIX="$HOME/.deepinwine/$BOTTLENAME"
 WECHAT_FONTS="$WINEPREFIX/drive_c/windows/Fonts"
-WECHAT_VER="3.6.0.18"
+WECHAT_VER="3.7.0.29"
 EXEC_PATH="c:/Program Files/Tencent/WeChat/WeChat.exe"
 EXEC_FILE="$WINEPREFIX/drive_c/Program Files/Tencent/WeChat/WeChat.exe"
 START_SHELL_PATH="/opt/deepinwine/tools/run_v4.sh"
@@ -35,8 +35,15 @@ export LD_LIBRARY_PATH=/opt/apps/$DEB_PACKAGE_NAME/files/lib32
 
 export WINEPREDLL="$ARCHIVE_FILE_DIR/dlls"
 
+msg()
+{
+	ECHO_LEVEL=("\033[1;32m==> " "\033[1;31m==> ERROR: ")
+	echo -e "${ECHO_LEVEL[$1]}\033[1;37m$2\033[0m"
+}
+
 OpenWinecfg()
 {
+    msg 0 "Launching winecfg with $APPRUN_CMD in $WINEPREFIX ..."
     env WINEPREFIX=$WINEPREFIX $APPRUN_CMD winecfg
 }
 
@@ -44,21 +51,26 @@ DeployApp()
 {
     # backup fonts
     if [ -d "$WECHAT_FONTS" ];then
+        msg 0 "Backing up fonts ..."
         mkdir -p $HOME/.deepinwine/.wechat_tmp
         cp $WECHAT_FONTS/* $HOME/.deepinwine/.wechat_tmp/
     fi
 
-    # re-deploy bottle
+    # deploy bottle
+    msg 0 "Deploying $WINEPREFIX ..."
     rm -rf "$WINEPREFIX"
     # run installer
+    msg 0 "Launching $WECHAT_INSTALLER_PATH ..."
     env WINEDLLOVERRIDES="winemenubuilder.exe=d" $START_SHELL_PATH $BOTTLENAME $APPVER "$WECHAT_INSTALLER_PATH" "$@"
 
     # restore fonts
     if [ -d "$HOME/.deepinwine/.wechat_tmp" ];then
+        msg 0 "Restoring fonts ..."
         cp -n $HOME/.deepinwine/.wechat_tmp/* $WECHAT_FONTS/
         rm -rf "$HOME/.deepinwine/.wechat_tmp"
     fi
     touch $WINEPREFIX/reinstalled
+    msg 0 "Creating $WINEPREFIX/PACKAGE_VERSION ..."
     cat /opt/apps/$DEB_PACKAGE_NAME/files/files.md5sum > $WINEPREFIX/PACKAGE_VERSION
 }
 
@@ -78,12 +90,16 @@ Run()
         else
             # missing exec file
             if [ ! -f "$EXEC_FILE" ];then
+                msg 1 "Missing $EXEC_FILE, re-deploying ..."
                 DeployApp
+                exit 0
             fi
 
             if [ -z "${EXEC_PATH##*.lnk*}" ];then
+                msg 0 "Launching  $EXEC_PATH lnk file ..."
                 $START_SHELL_PATH $BOTTLENAME $APPVER "C:/windows/command/start.exe" "/Unix" "$EXEC_PATH" "$@"
             else
+                msg 0 "Launching  $EXEC_PATH ..."
                 $START_SHELL_PATH $BOTTLENAME $APPVER "$EXEC_PATH" "$@"
             fi
         fi
